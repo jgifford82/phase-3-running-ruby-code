40764f005333d7f5e912c3e8c90db9fb3d52e8d0
Jennifer Gifford <jgifford82@gmail.com>
2022-10-04 19:13:24 -0600

    commit tree 9a2910d2bee0b980dbe847e4665a1b191def6c0e

---
+++ app.rb
@@ -0,0 +1,23 @@
+# app.rb
+
+# put ruby app.rb in the terminal to print output
+
+# both of these do the same thing. parentheses are optional. 1st syntax is preferred for looks
+puts "Hello World!"
+# puts("Hello world!")
+
+# puts and print will do almost the same thing, but puts includes a line break at end of string
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+
+# this prints a string
+# puts [1, 2, 3]
+
+# this prints an array
+p [1, 2, 3]
+
+# use pp to print complex data in a nice view
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+print "Pass this test, please."
